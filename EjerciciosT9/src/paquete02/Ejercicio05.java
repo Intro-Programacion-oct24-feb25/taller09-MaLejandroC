ae0d35e07ed0b458999d084d355f9545ab3798eb
MalejandroC <macarrion44@utpl.edu.ec>
2024-12-12 13:49:37 -0500

    Problemas

--- EjerciciosT9/src/paquete02/Ejercicio05.java
+++ EjerciciosT9/src/paquete02/Ejercicio05.java
@@ -17,6 +17,24 @@ public class Ejercicio05 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        String[] dias= {"Lunes","Martes","Miercoles","Jueves","Viernes"};
+        double[] ventas = new double[5]; 
+        
+        for (int i = 0 ; i < dias.length; i++) {
+            
+            System.out.printf("Ingrese el valor de ventas del %s: $", dias[i]);
+            ventas[i] = entrada.nextDouble();
+        }
+        
+        for (int i = 0 ; i < dias.length; i++) {
+            System.out.printf("%s $%.0f\n",dias[i],ventas[i]);
+            
+        }
+        
+        
       
 
     }
