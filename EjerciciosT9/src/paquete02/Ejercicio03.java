3508d57669b7313c049c35b89b0522979ddc1c8f
MalejandroC <macarrion44@utpl.edu.ec>
2024-12-12 12:33:06 -0500

    Problemas

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -17,7 +17,31 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
+        int suma = 0;
+        double promedio;
+        int arriba = 0;
+        int debajo = 0;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+
+        }
+
+        promedio = (double) suma / arreglo.length;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] < promedio) {
+                arriba = arriba + 1;
+            } else {
+                debajo = debajo + 1;
+            }
+        }
         
+        System.out.printf("Media aritmetica: %.2f\narriba de la media "
+                + "aritmética: %d\ndebajo de la medía aritmética: %d\n", 
+                promedio, 
+                arriba, 
+                debajo);
     }
 
 }
